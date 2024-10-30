7ff7dbf73c3da1a746d97a395cbd8ceb8751ad82
Destrifer <destrifer@yandex.ru>
2024-10-30 23:32:33 +0300

    try deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import icon from 'astro-icon';
 
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   output: 'server',
   integrations: [icon(), tailwind()],
+  adapter: netlify(),
 });
